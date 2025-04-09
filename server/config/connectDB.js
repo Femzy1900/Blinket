bdb7d6902bf282565ebb6450f17bd1d1735b48ad
Femzy1900 <adedokunfemi14@gmail.com>
2025-04-09 23:34:42 +0100

    Getting started with email verification

---
+++ server/config/connectDB.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+import dotenv from "dotenv";
+dotenv.config()
+
+if(!process.env.MONGO_URL) {
+    throw new Error(
+        "Please provide MONGODB_URL in the .env file"
+    )
+}
+
+async function connectDB() {
+    try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("Connect DB successfully")
+    } catch (error) {
+        console.log("Mongodb connection error", error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
