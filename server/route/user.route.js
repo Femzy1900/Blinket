4e7c2f21448d8c952b14e69f94f388dd1f56d9bf
Femzy1900 <adedokunfemi14@gmail.com>
2025-04-20 23:28:16 +0100

    login controller

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -4,5 +4,8 @@ import { registerUserController } from '../controller/user.controller.js';
 const userRouter = Router();
 
 userRouter.post('/register', registerUserController);
+// userRouter.post('/verify-email', verifyEmailController)
+
+
 
 export default userRouter;
