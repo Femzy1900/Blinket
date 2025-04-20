fb8453b37e0f9ce7aa569656d6b99223a18b0081
Femzy1900 <adedokunfemi14@gmail.com>
2025-04-21 00:12:29 +0100

    fix: update refresh token generation to use correct secret key

--- server/utils/generateRefreshToken.js
+++ server/utils/generateRefreshToken.js
@@ -2,11 +2,11 @@ const jwt = require("jsonwebtoken");
 import UserModel from "../models/user.model";
 
 const generateRefreshToken = async (userId) => {
-  const token = jwt.sign({ id: userId }, process.env.SECRET_KEY_ACCESS_TOKEN, {
+  const token = jwt.sign({ id: userId }, process.env.SECRET_KEY_REFRESH_TOKEN, {
     expiresIn: "30d",
   });
 
-  const updateRefreshToken = await UserModel.updateOne(
+  const updateRefreshTokenUser = await UserModel.updateOne(
     { _id: userId },
     {
         refreshToken: token,
