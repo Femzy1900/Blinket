55a6561fddc5716bf5f432569bb847d65e4a4b5c
Femzy1900 <adedokunfemi14@gmail.com>
2025-04-21 10:16:23 +0100

    feat: add logout route and integrate authentication middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,7 @@
 const auth = (req, res,next) => {
     try {
         const token = req.cookies.accessToken || req?.headers?.authorization?.split(" ")[1]    
-        
+        console.log(token)
     } catch (error) {
         return res.status(500).json({
             message: error.message || error,
--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,11 +1,13 @@
 import {Router} from 'express';
-import { registerUserController, loginController } from '../controller/user.controller.js';
+import { registerUserController, loginController, logoutController } from '../controller/user.controller.js';
+import { auth } from '../middleware/auth.middleware.js';
 
 const userRouter = Router();
 
 userRouter.post('/register', registerUserController);
 // userRouter.post('/verify-email', verifyEmailController)
 userRouter.post('/login', loginController)
+userRouter.get('logout', auth, logoutController)
 
 
 export default userRouter;
