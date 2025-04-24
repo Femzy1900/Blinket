6a43ee24dc7429050992bba81d98b235bb769f31
Femzy1900 <adedokunfemi14@gmail.com>
2025-04-24 22:47:55 +0100

    feat: add RESEND_API key to environment variables and enable verify email route

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -69,37 +69,37 @@ export async function registerUserController(req, res){
     }
 }
 
-// export async function verifyEmailController(request,response){
-//     try {
-//         const { code } = request.body
+export async function verifyEmailController(request,response){
+    try {
+        const { code } = request.body
 
-//         const user = await UserModel.findOne({ _id : code})
+        const user = await UserModel.findOne({ _id : code})
 
-//         if(!user){
-//             return response.status(400).json({
-//                 message : "Invalid code",
-//                 error : true,
-//                 success : false
-//             })
-//         }
+        if(!user){
+            return response.status(400).json({
+                message : "Invalid code",
+                error : true,
+                success : false
+            })
+        }
 
-//         const updateUser = await UserModel.updateOne({ _id : code },{
-//             verify_email : true
-//         })
+        const updateUser = await UserModel.updateOne({ _id : code },{
+            verify_email : true
+        })
 
-//         return response.json({
-//             message : "Verify email done",
-//             success : true,
-//             error : false
-//         })
-//     } catch (error) {
-//         return response.status(500).json({
-//             message : error.message || error,
-//             error : true,
-//             success : true
-//         })
-//     }
-// }
+        return response.json({
+            message : "Verify email done",
+            success : true,
+            error : false
+        })
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : true
+        })
+    }
+}
 
 //login controller
 
--- server/route/user.route.js
+++ server/route/user.route.js
@@ -6,7 +6,7 @@ import { upload } from '../middleware/upload.middleware.js';
 const userRouter = Router();
 
 userRouter.post('/register', registerUserController);
-// userRouter.post('/verify-email', verifyEmailController)
+userRouter.post('/verify-email', verifyEmailController)
 userRouter.post('/login', loginController)
 userRouter.get('logout', auth, logoutController)
 userRouter.put('upload-avatar', auth, upload.single('avatar'), uploadAvatar  )
