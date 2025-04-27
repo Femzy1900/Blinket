a79e0dc6e5be653b9215d1d12b170b8a717d4b4c
Femzy1900 <adedokunfemi14@gmail.com>
2025-04-27 23:20:07 +0100

    new

---
+++ client/src/components/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { IoSearch } from "react-icons/io5";
+
+const Search = () => {
+  return (
+    <div className='w-full min-w-[300px] lg:min-w-[420px] h-12 rounded-lg border'>
+        <IoSearch/>
+    </div>
+  )
+}
+
+export default Search
