ad543b8125b7a58c84b9a3fc85426c74255541b5
Femzy1900 <adedokunfemi14@gmail.com>
2025-04-28 01:45:33 +0100

    feat: add custom hook useMobile for responsive design handling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,40 +1,78 @@
 import React from "react";
 import logo from "../assets/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Search from "./Search";
+import { FaRegUserCircle } from "react-icons/fa";
+import useMobile from "../hooks/useMobile";
+import {BsCart4} from 'react-icons/bs';
+import {TiShoppingCart} from 'react-icons/ti'
 
 const Header = () => {
+  const [isMobile ]= useMobile()
+  const location = useLocation()
+  const navigate = useNavigate()
+  const isSearchPage = location.pathname === "/search"
 
-
+  const redirectToLoginPage = ()=>{
+    navigate("/login")
+  }
   return (
-    <header className="h-20 shadow-md sticky top-0 ">
-      <div className="container mx-auto flex items-center h-full px-2 justify-between">
-        <div className="h-full">
-          {/* logo */}
-          <Link to={"/"} className="h-full flex justify-center items-center">
-            <img
-              src={logo}
-              width={170}
-              height={60}
-              alt="logo"
-              className="hidden lg:block"
-            />
-            <img
-              src={logo}
-              width={120}
-              height={60}
-              alt="logo"
-              className="lg:hidden"
-            />
-          </Link>
-        </div>
-        {/**Search */}
-        <div className="">
-          <Search />
-        </div>
+    <header className="h-20 shadow-md sticky top-0 z-40 flex flex-col justify-center gap-1 bg-white">
+      {
+        !(isSearchPage && isMobile) && (
+          <div className="container mx-auto flex items-center px-2 justify-between">
+            <div className="h-full">
+              {/* logo */}
+              <Link to={"/"} className="h-full flex justify-center items-center">
+                <img
+                  src={logo}
+                  width={170}
+                  height={60}
+                  alt="logo"
+                  className="hidden lg:block"
+                />
+                <img
+                  src={logo}
+                  width={120}
+                  height={60}
+                  alt="logo"
+                  className="lg:hidden"
+                />
+              </Link>
+            </div>
+            {/**Search */}
+            <div className="hidden lg:block">
+              <Search />
+            </div>
 
-        {/* login and my cart */}
-        <div className="">login</div>
+            {/* login and my cart */}
+            <div className="">
+              <button className='text-neutral-600 lg:hidden'>
+                <FaRegUserCircle size={26}/>
+              </button>
+
+              {/* Desktop */}
+              <div  className="hidden lg:flex items-center gap-10">
+                <button onClick={redirectToLoginPage}>
+                  login
+                </button>
+                <button className="flex items-center gap-2 bg-green-800 hover:bg-green-700 px-2 py-3 rounded text-white">
+                  {/* add to card icons */}
+                  <div className="animate-bounce">
+                    <BsCart4 size={30}/>
+                  </div>
+                  <div className="font-semibold">
+                    <p>My Cart</p>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+
+        )
+      }
+      <div className='container mx-auto px-2 lg:hidden'>
+            <Search/>
       </div>
     </header>
   );
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -2,11 +2,15 @@ import React, {useState, useEffect} from "react";
 import { IoSearch } from "react-icons/io5";
 import { useLocation, useNavigate } from "react-router-dom";
 import { TypeAnimation } from "react-type-animation";
+import useMobile from "../hooks/useMobile";
+import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 
 const Search = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const [isSearchPage, setIsSearchPage] = useState(false);
+  const [isMobile] = useMobile()
   // Check if the current page is the search page
   useEffect(() => {
     const isSearch = location.pathname === "/search"
@@ -18,9 +22,19 @@ const Search = () => {
   };
   return (
     <div className="w-full min-w-[300px] lg:min-w-[420px] h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-50 shadow-md group focus-within:border-primary-200">
-      <button className="flex justify-center items-center h-full p-3 group-focus-within:text-primary-200">
-        <IoSearch size={22} />
-      </button>
+      <div>
+            {
+                (isMobile && isSearchPage ) ? (
+                    <Link to={"/"} className='flex justify-center items-center h-full p-2 m-1 group-focus-within:text-primary-200 bg-white rounded-full shadow-md'>
+                        <FaArrowLeft size={20}/>
+                    </Link>
+                ) :(
+                    <button className='flex justify-center items-center h-full p-3 group-focus-within:text-primary-200'>
+                        <IoSearch size={22}/>
+                    </button>
+                )
+            }
+        </div>
       <div className="">
         {
           !isSearchPage ? (
@@ -56,9 +70,10 @@ const Search = () => {
             // in search page
             <div className="">
               <input
-                type="text"
-                placeholder="Search..."
-                className="w-full bg-transparent"
+                type='text'
+                placeholder='Search....'
+                autoFocus
+                className='bg-transparent w-full h-full outline-none'
               />
             </div>
           )
